e3e146556c0b3d0aa50ff75a09a739da0d8f2100
Devin Schwab <digidevin@gmail.com>
2019-05-10 16:12:34 -0400

    :sparkles: Working conan package

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,14 @@ import os
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
+    name = "better-enums"
+    version = "0.11.2"
+    description = "C++ compile-time enum to string, iteration, in a single header file."
     # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    author = "Bincrafters <bincrafters@gmail.com>"
+    topics = ("c++", "reflection", "enums")
+    url = "https://github.com/rhololkeolke/conan-better-enums"
+    homepage = "http://aantron.github.io/better-enums/"
+    author = "Devin Schwab <dschwab@andrew.cmu.edu>"
     license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
     no_copy_source = True
 
@@ -23,19 +23,30 @@ class LibnameConan(ConanFile):
     _source_subfolder = "source_subfolder"
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), sha256="Please-provide-a-checksum")
-        extracted_dir = self.name + "-" + self.version
+        os.makedirs(
+            os.path.join(self._source_subfolder, "include"), exist_ok=True)
 
-        #Rename to "source_folder" is a convention to simplify later steps
-        os.rename(extracted_dir, self._source_subfolder)
+        source_url = "https://raw.githubusercontent.com/aantron/better-enums"
+        enum_file_path = os.path.join(self._source_subfolder, "include",
+                                      "enum.h")
+        tools.download("{0}/{1}/enum.h".format(source_url, self.version),
+                       enum_file_path)
+        tools.check_sha256(
+            enum_file_path,
+            "e492f35736be931e3eb6d993e0074927ea3fa25044492d21a34368a00f35525e")
 
+        license_file_path = os.path.join(self._source_subfolder, "LICENSE")
+        tools.download("{0}/{1}/doc/LICENSE".format(source_url, self.version),
+                       license_file_path)
+        tools.check_sha256(
+            license_file_path,
+            "b213f959d7a07c2a161336723a284d795f8cf094e9146c05ed5de796eb9f2bb8")
 
     def package(self):
         include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(
+            pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         self.copy(pattern="*", dst="include", src=include_folder)
-
 
     def package_id(self):
         self.info.header_only()
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,6 +1,29 @@
 // #include <cpprest/json.h>
+#include <enum.h>
+
+BETTER_ENUM(Channel, int, Red=1, Green, Blue)
 
 int main()
 {
-    // const auto parsed_value = web::json::value::parse(U("-22"));
+  Channel c = Channel::_from_string("Red");
+  const char *s = c._to_string();
+
+  size_t n = Channel::_size();
+  for(Channel c : Channel::_values()) {
+    // run something
+  }
+
+  switch (c) {
+  case Channel::Red:
+    break;
+  case Channel::Green:
+    break;
+  case Channel::Blue:
+    break;
+  }
+
+
+  Channel c2 = Channel::_from_integral(3);
+
+  constexpr Channel c3 = Channel::_from_string("Blue");
 }
